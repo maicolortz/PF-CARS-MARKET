86371a23b939c6044ec0c6924c6b0ed4d46245cd
Emisael Kisler <ekisler@gmail.com>
2022-10-11 13:11:46 -0400

    creando model car

--- api/src/models/Car.js
+++ api/src/models/Car.js
@@ -4,9 +4,25 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('car', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
     },
+    marca: {
+      type: DataTypes.STRING, 
+    },
+    modelo: {
+      type: DataTypes.STRING,
+    },
+    color: {
+      type: DataTypes.STRING,
+    },
+    año: {
+      type: DataTypes.INTEGER,
+    },
+    km: {
+      type: DataTypes.INTEGER,
+    },
+    
   });
 };
