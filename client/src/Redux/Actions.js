ac6ff65d1e72d290be3d1b6090e1c3ecf018f6b1
maicolortz <maicolortiz28971@gmail.com>
2022-10-12 19:11:20 -0500

    Redux--config de acciones principales

---
+++ client/src/Redux/Actions.js
@@ -0,0 +1,88 @@
+import axios from "axios";
+
+export function getCars() {
+  return async function (dispatch) {
+    let json = await axios.get("/cars");
+    return dispatch({
+      type: "GET_CARS",
+      payload: json.data,
+    });
+  };
+}
+export function getCarForName(name) {
+  return async function (dispatch) {
+    try {
+      const { data } = await axios.get(`/cars?name=${name}`);
+      return dispatch({
+        type: "GET_CAR_FOR_NAME",
+        payload: data,
+      });
+    } catch (error) {
+      console.error(error.response.data);
+    }
+  };
+}
+
+
+export function getUsers() {
+  return async function (dispatch) {
+    const { data } = await axios.get("/users");
+    return dispatch({
+      type: "GET_USERS",
+      payload: data,
+    });
+  };
+}
+export function getUser(id) {
+    return async function (dispatch) {
+      const { data } = await axios.get(`/users/${id}`);
+      return dispatch({
+        type: "GET_USER",
+        payload: data,
+      });
+    };
+  }
+export function getCardDetail(id) {
+  return async function (dispatch) {
+    const { data } = await axios.get(`/cars/${id}`);
+    return dispatch({
+      type: "GET_CAR_DETAIL",
+      payload: data,
+    });
+  };
+}
+
+export function postCar(car) {
+  return async function () {
+    const d = await axios.post("/cars", car);
+    return d;
+  };
+}
+export function postUser(user) {
+    return async function () {
+      const d = await axios.post("/cars", user);
+      return d;
+    };
+  }
+export function OrderForPrice(payload) {
+  return {
+    type: "ORDER_FOR_PRICE",
+    payload,
+  };
+}
+export function OrderForName(payload) {
+  return {
+    type: "ORDER_FOR_NAME",
+    payload,
+  };
+}
+
+export function filterForModel(model) {
+  return async function (dispatch) {
+    const { data } = await axios.get(`/cars/?model=${model}`);
+    return dispatch({
+      type: "FILTER_FOR_MODEL",
+      payload: data,
+    });
+  };
+}
---
+++ client/src/Redux/Reducer.js
@@ -0,0 +1,76 @@
+const initialState = {
+    car: [],
+    user: [],
+    allCars: [],
+    allUsers:[],
+    carDetail: [],
+  };
+  export default function rootReducer(state = initialState, action) {
+    switch (action.type) {
+      case "GET_CARS":
+        return {
+          ...state,
+          allCars: action.payload,
+        };
+      case "GET_USER":
+        return {
+          ...state,
+          user: action.payload,
+        };
+        case "GET_USERS":
+        return {
+          ...state,
+          allUsers: action.payload,
+        };
+      case "GET_CAR_DETAIL":
+        return {
+          ...state,
+          cardDetail: action.payload,
+        };
+      case "GET_CAR_FOR_NAME":
+        let car=[];
+        car=action.payload==null?car:action.payload
+        return {
+          ...state,
+          car: car
+        };
+       
+      case "ORDER_FOR_PRICE":
+        let /* ordenWeight=state.car
+             let index=ordenWeight.findIndex(e=>e.id===232)
+             ordenWeight[index].weight.imperial='18'
+             state.car=ordenWeight; */
+          ordenPrice =
+            action.payload == "min"
+              ? state.car.sort((a, b) =>
+                  a.weight.metric.localeCompare(b.price, undefined, {
+                    numeric: true,
+                  })
+                )
+              : state.car.sort((a, b) =>
+                  b.weight.metric.localeCompare(a.price, undefined, {
+                    numeric: true,
+                  })
+                );
+        return {
+          ...state,
+          car: ordenPrice,
+        };
+      case "ORDER_FOR_NAME":
+        const ordeneds =
+          action.payload == "asc"
+            ? state.car.sort((a, b) => a.name.localeCompare(b.name))
+            : state.car.sort((a, b) => b.name.localeCompare(a.name));
+        return {
+          ...state,
+          car: ordeneds,
+        };
+      case "FILTER_FOR_MODEL":
+        return {
+          ...state,
+          car:action.payload
+        };
+      default:
+        return state;
+    }
+  };
