67a0ef1ae5ce577ff44438ba4860aabf390691fc
Luis Fernando Alvarez <fernando_0694@hotmail.com>
2022-10-16 00:10:27 -0500

    Arreglo de detalles en el Navbar y en las cards

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -8,37 +8,37 @@ import './Card.css';
 function Card({ id, image, descriptionShort, price, kilometres, transmition, year }) {
   return (
     <>
-      <div class="px-5 py-20 ">
-        <div class="w-96 h-full bg-white drop-shadow-2xl rounded-lg">
-          <img class="object-cover rounded-tl-lg rounded-tr-lg h-72 w-96" src={image} alt='Img not found' />
+      <div class="px-5 py-8 ">
+        <div class="md:w-40 h-full lg:w-52 xl:w-64 2xl:w-80  bg-white  shadow-2xl rounded-lg">
+          <img class=" rounded-tl-lg rounded-tr-lg md:h-40 w-96 xl:h-60 2xl:h-64" src={image} alt='Img not found' />
           <div class="px-5 py-4 space-y-2">
-            <div class="h-16 mb-4 ">
-              <h3 class="text-2xl font-semibold">{descriptionShort}</h3>
+            <div class="h-16 mb-1 md:mb-2  ">
+              <h3 class="md:text-sm lg:text-base xl:text-lg 2xl:text-xl font-semibold">{descriptionShort}</h3>
             </div>
             <p class="space-x-2 border-t pt-2 border-gray-300 text-gray-700">
-              <span class="text-2xl font-medium">Precio: {price}$</span>
+              <span class="md:text-base xl:text-lg 2xl:text-xl lg:mb-1 font-medium">Precio: {price}$</span>
             </p>
-            <div class="flex flex-col p-4 border-t border-gray-300 text-gray-700">
-              <div class="flex-1 pb-4 inline-flex items-center text-xl font-medium">
+            <div class="flex flex-col p-2 border-t border-gray-300 text-gray-700">
+              <div class="flex-1 pb-2 md:text-sm lg:text-lg  inline-flex items-center  font-medium">
                 <img src={icon1} class="mr-4" alt="" />
                 <p>Kilometros: {kilometres}Km</p>
               </div>
-              <div class="flex-1  pb-4 inline-flex items-center text-xl font-medium">
+              <div class="flex-1 pb-2 md:text-sm lg:text-lg  inline-flex items-center  font-medium">
                 <img src={icon2} class="mr-4" alt="" />
                 <p>Trasmision: {transmition}</p>
               </div>
-              <div class="flex-1 inline-flex items-center text-xl font-medium">
+              <div class="flex-1 md:text-sm lg:text-lg   inline-flex items-center  font-medium">
                 <img src={icon3} class="mr-4" alt="" />
                 <p>Año: {year}</p>
               </div>
             </div>
-            <div class="flex justify-between items-center pt-3 pb-2">
+            <div class="flex justify-between pt-0 px-6 pb-2">
               <Link to={`/cars/${id}`}>
-                <button class="px-3 py-2 text-white  bg-blue-900 hover:bg-blue-900 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-lg text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-900 dark:focus:ring-blue-900">
+                <button class="px-3 py-2 text-white  bg-blue-900 hover:bg-blue-900 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-lg lg:text-base xl:text-xl  text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-900 dark:focus:ring-blue-900">
                   Ver detalles
                 </button>
               </Link>
-              <a href="#top" title="Add to Favorites" class="text-5xl text-gray-300 hover:text-red-500 duration-300">&hearts;</a>
+              <a href="#top" title="Add to Favorites" class="md:text-5xl text-gray-300  hover:text-red-500 duration-300">&hearts;</a>
             </div>
           </div>
         </div>
--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -6,12 +6,13 @@ import Logo from './Logo CarMarket.png'
 export default function NavBar() {
     return (
         <nav className="contenedor-NavBar">
-            <img src={Logo} alt="img not found" width='120px' className="logo" />
-            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-lg px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Inicio</button>
+            <img src={Logo} alt="img not found" width='120px' className="logo" class="shadow-md shadow-black rounded-md" />
+            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none shadow-md shadow-black font-medium rounded-full text-lg px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-900 dark:focus:ring-blue-800">Inicio</button>
             <SearchBar />
             <div>
-                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-lg px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Ingresar</button>
-                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-lg px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Registrarse</button>
+                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none shadow-md shadow-black font-medium rounded-full text-lg px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-900 dark:focus:ring-blue-800">Ingresar</button>
+                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none shadow-md shadow-black font-medium rounded-full text-lg px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-900 dark:focus:ring-blue-800">Registrarse</button>
+                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none shadow-md shadow-black font-medium rounded-full text-lg px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-900 dark:focus:ring-blue-800">Crear Publicación</button>
             </div>
         </nav>
     )
