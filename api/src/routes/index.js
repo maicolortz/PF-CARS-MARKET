a5f82b8fca8d578744e6e1e727e39232805cc16a
Inguito <94653577+Inguito@users.noreply.github.com>
2022-10-20 19:29:21 -0300

    Implementación de Transaction con pasarela de pagos

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,11 +4,12 @@ const router = Router();
 const carsMain = require("./cars");
 const usersMain = require("./users");
 const payment =require("./payment")
+const transactionsMain = require("./transactions")
 
 router.use("/",payment)
 router.use("/cars",carsMain); 
 router.use("/users",usersMain); 
-
+router.use("/transactions",transactionsMain); 
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
