5092fc9b290a32e89999276a14cc8329db796bb0
maicolortz <107804493+maicolortz@users.noreply.github.com>
2022-10-30 11:28:53 -0500

    primeros toques visuales en admin

--- client/src/components/Dashboard/BienvenidoPanel.jsx
+++ client/src/components/Dashboard/BienvenidoPanel.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function BienvenidoPanel({estilos}) {
+function BienvenidoPanel({estilos, administrador}) {
   return (
     <div className='flex flex-col items-center' > 
-        <h1 className={estilos.titulo}>Bienvenido!!</h1>
+        <h1 className={administrador? estilos.tituloAdmin:estilos.titulo}>Bienvenido  {administrador? "Administrador":""} !!</h1>
         <h3 className={estilos.mensaje}>Desde Aqui podras <strong className='text-yellow-500'> <br/>Editar</strong> <br/> <strong className='text-blue-800'> Crear </strong>   <br/> o <strong className='text-orange-500'>Pausar</strong> <br/> tus Publicaciones</h3>
     </div>
   )
